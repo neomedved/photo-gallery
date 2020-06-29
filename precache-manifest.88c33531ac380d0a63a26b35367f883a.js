58da12d04f03949e9aa627040c5b05f11a608aa6
Artem Medvedev <at.medvedev@gmail.com>
2020-06-29 09:51:34 +0300

    Updates

---
+++ precache-manifest.88c33531ac380d0a63a26b35367f883a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69d7a92d94edc52549a83df8b2cfd01d",
+    "url": "/photo-gallery/index.html"
+  },
+  {
+    "revision": "3fb46f28aaa0a40cc3a6",
+    "url": "/photo-gallery/static/css/main.91081eed.chunk.css"
+  },
+  {
+    "revision": "e2cfc6473a65579f7418",
+    "url": "/photo-gallery/static/js/2.5e2f1f69.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/photo-gallery/static/js/2.5e2f1f69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fb46f28aaa0a40cc3a6",
+    "url": "/photo-gallery/static/js/main.22028b59.chunk.js"
+  },
+  {
+    "revision": "dea700b16e354df5ea7c",
+    "url": "/photo-gallery/static/js/runtime-main.2fb2e379.js"
+  },
+  {
+    "revision": "6422eac71f54ee5cd007dcf46fc0d22d",
+    "url": "/photo-gallery/static/media/roboto-medium-webfont.6422eac7.woff"
+  },
+  {
+    "revision": "58bf8e0bd1dd213bf745e9d748caf175",
+    "url": "/photo-gallery/static/media/roboto-regular-webfont.58bf8e0b.woff"
+  }
+]);
